40fbe589546e0268ad2f5751f55a4df6f9f63d36
shraddha-1 <shraddhadshine192@gmail.com>
2023-04-28 18:47:40 +0530

    login and create organization

--- resources/views/createorganization.blade.php
+++ resources/views/createorganization.blade.php
@@ -16,7 +16,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Login </title>
+    <title>Create Organization </title>
 
     <meta name="description" content="" />
 
@@ -60,18 +60,28 @@
                 
               </div>
               <!-- /Logo -->
-              <h4 class="mb-2">Welcome back! 👋</h4>
-              <p class="mb-4">Please sign-in to your account </p>
+              <h4 class="mb-2">Create Organization! 👋</h4>
 
               <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
                 <div class="mb-3">
-                  <label for="email" class="form-label">Email or Username</label>
+                  <label for="email" class="form-label"> Name</label>
                   <input
                     type="text"
                     class="form-control"
                     id="email"
                     name="email-username"
-                    placeholder="Enter your email or username"
+                    placeholder="Enter your Name"
+                    autofocus
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="email"
+                    name="email-username"
+                    placeholder="Enter your email "
                     autofocus
                   />
                 </div>
@@ -80,6 +90,7 @@
                     <label class="form-label" for="password">Password</label>
                     
                   </div>
+                  
                   <div class="input-group input-group-merge">
                     <input
                       type="password"
@@ -89,17 +100,39 @@
                       placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
                       aria-describedby="password"
                     />
-                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                    
                   </div>
                 </div>
+                <div class="mb-3 form-password-toggle">
+                  <div class="d-flex justify-content-between">
+                    <label class="form-label" for="password">Confirm Password</label>
+                    
+                  </div>
+                  
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    
+                  </div>
+                </div>
+                <div>
+                        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                      </div>
+
+
                 <div class="mb-3">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="remember-me" />
-                    <label class="form-check-label" for="remember-me"> Remember Me </label>
-                  </div>
+                    </div>
                 </div>
                 <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                  <button class="btn btn-primary d-grid w-100" type="submit">Create Organization</button>
                 </div>
               </form>
 
